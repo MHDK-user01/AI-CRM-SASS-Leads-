1b86404ba9cc111e67f5716dab6458fde567efa5
MHDK-user01 <companyacckhlaifamhd@gmail.com>
2026-09-11 11:04:19 +0300

    Add note model

---
+++ backend/models/Note.js
@@ -0,0 +1,2 @@
+import mongoose from 'mongoose';
+const schema=new mongoose.Schema({owner:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true,index:true},content:{type:String,required:true},lead:{type:mongoose.Schema.Types.ObjectId,ref:'Lead',default:null},contact:{type:mongoose.Schema.Types.ObjectId,ref:'Contact',default:null},pinned:{type:Boolean,default:false}},{timestamps:true}); export default mongoose.model('Note',schema);
